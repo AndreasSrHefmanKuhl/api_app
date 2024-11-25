76a9c7c5a9a0f267ea70287b7649818d4bc93629
AndreasKuhl <hefbolzen@gmail.com>
2024-11-25 12:49:42 +0100

    initial Commit + plus fin QuoteScreen

---
+++ api_app/lib/main.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const QuoteScreen());
+}
+
+class QuoteScreen extends StatelessWidget {
+  const QuoteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const JsonDecoder decoder = JsonDecoder();
+
+    const String jsonString = '''
+    {
+      "quote": "Success, as I see it, is a result, not a goal.",
+      "author": "Flaubert, George",
+      "category": "success"
+    }''';
+
+    final Map<String, dynamic> quotes = decoder.convert(jsonString);
+    final quote = quotes.toString();
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(quote),
+              const SizedBox(height: 25),
+              FloatingActionButton(onPressed: () {}),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
